9d09f6f8cb60093c6b320e44a5bf146d322cd349
canknbr <cankanbur1@gmail.com>
2023-04-03 21:58:24 +0300

    Initial Commit

---
+++ Weather App Animations/Weather_App_AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_App_AnimationsApp.swift
+//  Weather App Animations
+//
+//  Created by Can Kanbur on 3.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct Weather_App_AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
